954b7ea7b9e30af1bba199b15929e80a19a20f27
Fabian Kramm <kramm@covexo.com>
2018-05-30 11:11:58 +0200

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,20 @@
+/**
+ * app.js
+ *
+ * This is the entry file for the application, only setup and boilerplate
+ * code.
+ */
+
+// Needed for redux-saga es6 generator support
+import 'babel-polyfill';
+
+// Import all the third party stuff
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    Hello World!
+  </div>,
+  document.getElementById('app')
+);
